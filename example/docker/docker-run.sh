cef45c359ee190e064dfcfa048291e90ce40ba58
witfog <xzfwitfog@gmail.com>
2020-09-12 03:02:06 +0800

    fix: 🐛 Fix error path

--- example/docker/docker-run.sh
+++ example/docker/docker-run.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 docker run --name hoshino \
-    -v ../volumes/modules:/HoshinoBot/hoshino/modules \
-    -v ../volumes/config:/HoshinoBot/hoshino/config \
-    -v ../volumes/img:/HoshinoBot/img \
+    -v $PWD/../volumes/modules:/HoshinoBot/hoshino/modules \
+    -v $PWD/../volumes/config:/HoshinoBot/hoshino/config \
+    -v $PWD/../volumes/img:/HoshinoBot/img \
     -d ghcr.io/witfog/hoshinobot
